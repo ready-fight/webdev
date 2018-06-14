bad415f4ace752e7d5aef7ddad371777c46f9cd7
adrianwave <seraphzp@gmail.com>
2018-06-15 01:40:46 +0900

    aaa

---
+++ budgety/app.js
@@ -0,0 +1,91 @@
+var budgetController = (function() {
+    
+    var Expense  = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var Income  = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+    
+})();
+
+
+var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+var UIController = (function() {
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+        
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    
+        document.addEventListener('keypress', function(event) {
+           if(event.keyCode === 13) {
+               ctrlAddItem();
+           }
+        });
+    }
+    
+    var ctrlAddItem = function() {
+        var input = UICtrl.getInput();
+        console.log(input);
+    };
+    
+    return {
+        init: function() {
+            console.log('Application has started');
+            setupEventListeners();
+        }
+    };
+    
+})(budgetController, UIController);
+
+controller.init();
