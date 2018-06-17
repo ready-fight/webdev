b325b27c448668afd4d330f68ea82d79a94b5c2d
adrianwave <seraphzp@gmail.com>
2018-06-18 02:20:23 +0900

    update

--- budgety/app.js
+++ budgety/app.js
@@ -56,6 +56,19 @@ var budgetController = (function() {
             return newItem;
             
         },
+        deleteItem: function(type, id){
+            var ids, index;
+            ids = data.allItems[type].map(function(current){
+                return current.id;
+            });
+            
+            index = ids.indexOf(id);
+            
+            if(index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+            
+        },
         
         calculateBudget: function() {
           // Calculate total income & expenses
@@ -221,6 +234,7 @@ var controller = (function(budgetCtrl, UICtrl) {
             type = splitID[0];
             ID = splitID[1];
             
+            budgetCtrl.deleteItem(type, ID);
             
         }
     };
