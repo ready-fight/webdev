bfa9e0c5b45a5fc2b8135c7b17a7c8796f60d4dd
Adrian Ruiz <r-adrian@confiance-inc.co.jp>
2018-10-24 12:30:10 +0900

    てすと

---
+++ messenger/signup.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create new account</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/fontawesome/css/all-min.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,900" rel="stylesheet">
+</head>
+<body>
+    <div class="signup-container">
+    
+    </div>
+    
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+</body>
+</html>
