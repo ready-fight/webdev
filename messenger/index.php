2ca43e327015f28ff3e7e201b65691a13d13c88b
Adrian Ruiz <r-adrian@confiance-inc.co.jp>
2018-10-26 18:15:26 +0900

    test

--- messenger/index.php
+++ messenger/index.php
@@ -1,1 +1,27 @@
-<?php phpinfo() ?>
+<?php include 'init.php'; ?>
+
+<?php if(!isset($_SESSION['user_id'])): ?>
+    <?php header("location: login.php"); ?>
+<?php endif; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <?php include 'components/css.php'; ?>
+</head>
+<body>
+    <?php if(isset($_SESSION['loader'])): ?>
+    <div class="loader-area">
+        <div class="loader">
+            <div class="loader-item">
+            </div>
+        </div>
+    </div>
+    <?php unset($_SESSION['loader']); ?>
+    <?php endif ?> 
+</body>
+</html>
--- messenger/init.php
+++ messenger/init.php
@@ -3,7 +3,7 @@
 session_start();
 spl_autoload_register(function($class_name) {
 
-    include "classes/$class_name.php";
+    include 'classes/$class_name.php';
 
     // classes/db.php
 });
