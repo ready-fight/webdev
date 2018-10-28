cb5fcc655174313d25bb899bc5157e4f17ad3a4f
Adrian Ruiz <seraphzp@gmail.com>
2018-10-29 00:18:33 +0900

    commit

--- slotify/register.php
+++ slotify/register.php
@@ -18,73 +18,78 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo Constants::$title; ?></title>
+    <link rel="stylesheet" type="text/css" href="assets/css/register.css">
 </head>
 <body>
-    <div id="inputContainer">
-        <form action="register.php" id="loginForm" method="POST">
-            <h2>Login to your account</h2>
-            
-            <p>
-                <?php if(isset($_POST['loginButton'])) : ?>
-                    <?php echo $account->getError() ?>
-                <?php endif; ?>
-                <label for="loginUsername">Username: </label>
-                <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. adrianRuiz" value="<?php $account->getInputValue('loginUsername'); ?>" required />
-            </p>
-            <p>
-                <label for="loginPassword">Password: </label>
-                <input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required />
-            </p>
+    <div id="background">
+        <div id="loginContainer">
+            <div id="inputContainer">
+                <form action="register.php" id="loginForm" method="POST">
+                    <h2>Login to your account</h2>
+                    
+                    <p>
+                        <?php if(isset($_POST['loginButton'])) : ?>
+                            <?php echo $account->getError() ?>
+                        <?php endif; ?>
+                        <label for="loginUsername">Username: </label>
+                        <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. adrianRuiz" value="<?php $account->getInputValue('loginUsername'); ?>" required />
+                    </p>
+                    <p>
+                        <label for="loginPassword">Password: </label>
+                        <input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required />
+                    </p>
 
-            <button type="submit" name="loginButton">LOG IN</button>
+                    <button type="submit" name="loginButton">LOG IN</button>
 
-        </form>
+                </form>
 
-        <form action="register.php" id="registerForm" method="POST">
+                <form action="register.php" id="registerForm" method="POST">
 
-            <h2>Create your account</h2>
-            
-            <p>
-            <?php if(isset($_POST['registerButton'])) : ?>
-                <?php echo $account->getError() ?>
-            <?php endif; ?>
-                <label for="username">Username: </label>
-                <input id="username" name="username" type="text" placeholder="e.g. adrianRuiz" value="<?php $account->getInputValue('username'); ?>" required />
-            </p>
+                    <h2>Create your account</h2>
+                    
+                    <p>
+                    <?php if(isset($_POST['registerButton'])) : ?>
+                        <?php echo $account->getError() ?>
+                    <?php endif; ?>
+                        <label for="username">Username: </label>
+                        <input id="username" name="username" type="text" placeholder="e.g. adrianRuiz" value="<?php $account->getInputValue('username'); ?>" required />
+                    </p>
 
-            <p>
-                <label for="firstName">First Name: </label>
-                <input id="firstName" name="firstName" type="text" placeholder="e.g. Adrian" value="<?php $account->getInputValue('firstName'); ?>" required />
-            </p>
+                    <p>
+                        <label for="firstName">First Name: </label>
+                        <input id="firstName" name="firstName" type="text" placeholder="e.g. Adrian" value="<?php $account->getInputValue('firstName'); ?>" required />
+                    </p>
 
-            <p>
-                <label for="lastName">Last Name: </label>
-                <input id="lastName" name="lastName" type="text" placeholder="e.g. Ruiz" value="<?php $account->getInputValue('lastName'); ?>" required />
-            </p>
+                    <p>
+                        <label for="lastName">Last Name: </label>
+                        <input id="lastName" name="lastName" type="text" placeholder="e.g. Ruiz" value="<?php $account->getInputValue('lastName'); ?>" required />
+                    </p>
 
-            <p>
-                <label for="email">Email: </label>
-                <input id="email" name="email" type="email" placeholder="e.g. aRuiz@gmail.com" value="<?php $account->getInputValue('email'); ?>" required />
-            </p>
+                    <p>
+                        <label for="email">Email: </label>
+                        <input id="email" name="email" type="email" placeholder="e.g. aRuiz@gmail.com" value="<?php $account->getInputValue('email'); ?>" required />
+                    </p>
 
-            <p>
-                <label for="email2">Confirm Email: </label>
-                <input id="email2" name="email2" type="email" placeholder="e.g. aRuiz@gmail.com" value="<?php $account->getInputValue('email2'); ?>" required />
-            </p>
+                    <p>
+                        <label for="email2">Confirm Email: </label>
+                        <input id="email2" name="email2" type="email" placeholder="e.g. aRuiz@gmail.com" value="<?php $account->getInputValue('email2'); ?>" required />
+                    </p>
 
-            <p>
-                <label for="password">Password: </label>
-                <input id="password" name="password" type="password" placeholder="Your password" required />
-            </p>
+                    <p>
+                        <label for="password">Password: </label>
+                        <input id="password" name="password" type="password" placeholder="Your password" required />
+                    </p>
 
-            <p>
-                <label for="password2">Confirm Password: </label>
-                <input id="password2" name="password2" type="password" placeholder="Your password" required />
-            </p>
+                    <p>
+                        <label for="password2">Confirm Password: </label>
+                        <input id="password2" name="password2" type="password" placeholder="Your password" required />
+                    </p>
 
-            <button type="submit" name="registerButton">SIGN UP</button>
-
-        </form>
+                    <button type="submit" name="registerButton">SIGN UP</button>
+                </form>
+            </div>
+            <div id="clearfix"></div>
+        </div>
     </div>
 </body>
 </html>
