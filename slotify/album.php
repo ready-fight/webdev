0d750d569524751bb63f34c58061a43ce894c0e6
Adrian Ruiz <seraphzp@gmail.com>
2018-11-05 08:11:48 +0900

    commit

--- slotify/album.php
+++ slotify/album.php
@@ -1,2 +1,27 @@
-<?php require 'includes/header.php' ?>
-<?php require 'includes/footer.php' ?>
+<?php 
+    require 'includes/header.php'; 
+    if(isset($_GET['id'])) {
+        $albumId = $_GET['id'];
+    } else {
+        header("Location: index.php");
+    }
+
+    $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
+    $album = mysqli_fetch_array($albumQuery);
+
+    $artistId = $album['artist'];
+
+    $artistQuery = mysqli_query($con, "SELECT * FROM artists where id='$artistId'");
+    $artist = mysqli_fetch_array($artistQuery);
+
+    $artistId = $album['artist'];
+
+    $artist = new Artist($con, $artistId);
+
+    echo $album['title'] . "<br />";
+    echo $artist['name'];
+
+?>
+
+
+<?php require 'includes/footer.php'; ?>
--- slotify/includes/config.php
+++ slotify/includes/config.php
@@ -5,7 +5,7 @@
 
     $timezone = date_default_timezone_set("Asia/Tokyo");
 
-    $con = mysqli_connect("localhost", "root", "root", "slotify");
+    $con = mysqli_connect("localhost", "root", "", "slotify");
 
     if(mysqli_connect_errno()) {
         echo "Failed to connect. Error code: " . mysqli_connect_errno();
