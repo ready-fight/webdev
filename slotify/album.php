609787ab9a5b17bad9292157b157e534261b81e5
Adrian Ruiz <r-adrian@confiance-inc.co.jp>
2018-11-06 17:57:33 +0900

    commit

--- slotify/album.php
+++ slotify/album.php
@@ -8,13 +8,21 @@
     }
 
     $album = new Album($con, $albumId);
-
     $artist = $album->getArtist();
-
-    echo $album->getTitle() . "<br />";
-    echo $artist->getName();
-
 ?>
+
+<div class="entityInfo">
+    
+    <div class="leftSection">
+        <img src="<?php echo $album->getArtworkPath(); ?>" alt="" />
+    </div>
+
+    <div class="rightSection">
+        <h2><?php echo $album->getTitle(); ?></h2>
+        <span>By <?php echo $artist->getName(); ?></span>
+    </div>
+
+</div>
 
 
 <?php require 'includes/footer.php'; ?>
