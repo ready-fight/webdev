a4f26ea36d0f8227e89c4fb6bd752d166a9a1d37
Adrian Ruiz <seraphzp@gmail.com>
2018-11-09 01:20:36 +0900

    commit

--- slotify/album.php
+++ slotify/album.php
@@ -29,11 +29,34 @@
         <?php 
             $songIdArray = $album->getSongIds();
 
+            $i = 1;
             foreach($songIdArray as $songId) {
                 $albumSong = new Song($con, $songId);
                 $albumArtist = $albumSong->getArtist();
 
-                echo "<li></li>";
+                echo "
+                <li class='trackListRow'>
+                    <div class='trackCount'>
+                        <img class='play' src='assets/images/icons/play-white.png' />
+                        <span class='trackNumber'>$i</span>
+                    </div>
+
+                    <div class='trackInfo'>
+                        <span class='trackName'>" . $albumSong->getTitle() . "</span>
+                        <span class='artistName'>" . $albumArtist->getName() . " </span>
+                    </div>
+
+                    <div class='trackOptions'>
+                        <img class='optionsButton' src='assets/images/icons/more.png' />
+                    </div>
+
+                    <div class='trackDuration'>
+                        <span class='duration'>" . $albumSong->getDuration() . "</span>
+                    </div>
+
+                </li>";
+                $i++;
+
             }
 
         ?>
