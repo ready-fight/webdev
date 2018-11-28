e35e3c5d3467f4427ad1426972011e0eb90d85d8
Adrian Ruiz <r-adrian@confiance-inc.co.jp>
2018-11-28 17:11:52 +0900

    commit

---
+++ slotify/yourMusic.php
@@ -0,0 +1,13 @@
+<?php
+    require 'includes/includedFiles.php';
+?>
+
+<div class="playlistsContainer">
+    <div class="gridViewContainer">
+        <h2>PLAYLISTS</h2>
+
+        <div class="buttonItems">
+            <button class="button green">NEW PLAYLIST</button>
+        </div>
+    </div>
+</div>
