4f87f816e379cba59026e349a37d4f60122100b2
Adrian Ruiz <r-adrian@confiance-inc.co.jp>
2018-11-30 16:44:05 +0900

    commit

---
+++ portfolio/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 5000,
+        pagination: false
+    });
+
+    var typed = new Typed(".typed", {
+        strings: ["Software Engineer", "Web Developer", "Game Programmer"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+});
