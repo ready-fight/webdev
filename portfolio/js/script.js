c0baf9645eaea9d5991a74ca39526dcd54e97eaa
ready-fight <seraphzp@gmail.com>
2018-12-02 16:47:41 +0900

    commit

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -13,4 +13,44 @@ $(document).ready(function() {
         startDelay: 1000,
         showCursor: false
     });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items: 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            }
+        }
+    });
+
+    var skillsTopOffSet = $(".skillsSection").offset().top;
+
+    console.log(skillsTopOffSet);
+
+    $(window).scroll(function(){
+        if(window.pageYOffset >  skillsTopOffSet - $(window).height() + 200) {
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        }
+    });
+
 });
